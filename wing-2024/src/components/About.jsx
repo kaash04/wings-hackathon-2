ea64e09e9bacfba42b1bf73719d765ec40c65aaa
Akash <2k21akash@gmail.com>
2024-01-20 18:01:33 +0530

    6

--- wing-2024/src/components/About.jsx
+++ wing-2024/src/components/About.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <div className="content">
+        <div className="txt">
+          {/* <h1 style="color: white; font-weight: 900;font-size: 5rem;">About</h1> */}
+          <h1 className='ramu'>About</h1>
+
+          <p>
+            Welcome to our website, where innovation meets academia! We take pride in showcasing the collaborative spirit and creative prowess of students from diverse universities who have come together to embark on integrated projects. Our platform serves as a hub for these ambitious individuals, fostering a dynamic space where ideas converge and innovative solutions take shape.</p>
+
+        </div>
+        <div className="img">
+          <div className="social-icons">
+            <img src="https://cdn.discordapp.com/attachments/1010996276202065941/1198240464910962790/social-icon4.png?ex=65be2f65&is=65abba65&hm=740a88abd6da4815727e1201a77325e15c39a64ef387b8914720ad8761cac04a&" alt="fb" />
+            <img src="https://cdn.discordapp.com/attachments/1010996276202065941/1198240505193054208/social-icon3.png?ex=65be2f6e&is=65abba6e&hm=cd75ccd17e99f03b3d5b6ee1ad1a9b8684921ee5b2067ff387601e63ed93d589&" alt="pinrest" />
+            <img src="https://cdn.discordapp.com/attachments/1010996276202065941/1198240539372421120/social-icon5.png?ex=65be2f76&is=65abba76&hm=2db55f6053bae2da2ea6bfddc9d5867502e6f9b7205e797da51f92a5bfd4e551&" alt="twitter" />
+            <img src="https://cdn.discordapp.com/attachments/1010996276202065941/1198240585337802784/social-icon2.png?ex=65be2f81&is=65abba81&hm=c40044bf9666f9e5af733386a6ed880a04fafbb0485d62008940dbe9cfb38617&" alt="linkedin" />
+            <img src="https://cdn.discordapp.com/attachments/1010996276202065941/1198240610281341019/social-icon1.png?ex=65be2f87&is=65abba87&hm=8a4ed5072331548f0a6e8b02932a22d1cf70134479d4f5911a44422f7da7a9db&" alt="insta" />
+          </div>
+          <img className="email-icon" src="https://cdn.discordapp.com/attachments/1010996276202065941/1198240426155577406/email-icon.png?ex=65be2f5b&is=65abba5b&hm=18b5ce9d167fb08f98a5de49fc9f8bd8c341d127f54bcb68846cb0e3a7e68740&" alt="mail" />
+        </div>
+      </div>
+    </>
   )
 }
 
